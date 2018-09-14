6f13c3bc3e21d7892623f113dc6bb6540a75bfde
Lauren Days <lmchambers0420@gmail.com>
2018-09-14 18:17:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,18 +1,5 @@
 def turn(board) 
-  puts "Please enter 1-9:"
-  i = gets.strip 
-  index = input_to_index(i) 
-  m = valid_move?(board, index) 
-  if m == true move(board, index, player_token) 
-  else m == false 
-  until m == true 
-  puts "Sorry, that was an invalid move. Please enter 1-9:"
-  i = gets.strip 
-  index = input_to_index(i) 
-  m = valid_move?(board, index) move(board, index, player_token) 
-  end 
-  end 
-  display_board(board) 
+  
 end
 
 def input_to_index(input)
