cf5d96c9507db3f99d2f14ea09c53fb7eeb0a007
Lauren Days <lmchambers0420@gmail.com>
2018-09-14 17:40:16 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,39 @@
+def turn(board)
+  puts "Please enter 1-9:"
+    userInput = gets.strip
+    index = input_to_index(userInput)
+    if !valid_move?(board, index)
+      turn(board)
+    else 
+      move(board, index, current_player(board))
+    end
+  display_board(board)
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+def valid_move?(board, index)
+ index.between?(0,8) && !position_taken?(board, index)
+  end
+  
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+      taken = false
+    elsif board[index] == "X" || board[index] == "O"
+      taken = true
+  end
+end
+
+def move(board, index, symbol)
+  board[index] = symbol
+end
